c62e5915752ddd257835e695ef07c27088ec253f
whoknowscode <luispcode@gmail.com>
2021-08-24 10:30:00 -0400

    env variables

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ProvideUser } from './services/context/UserState';
 
+console.log(process.env.NODE_ENV);
+
 ReactDOM.render(
   <React.StrictMode>
     <ProvideUser>
